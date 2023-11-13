b710eba3037af5ce7ab91bc74415a2a57c66be35
Litsea <litsea52@gmail.com>
2023-11-13 12:34:01 +0900

    add: HomeビューにToolListセクションを追加

---
+++ src/components/02_sections/home/ToolList.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+  import ToolListItem from '@/components/03_parts/home/ToolListItem.vue'
+
+  const itemAttrsList = [
+    {
+      path: '/scales',
+      icon: '/src/assets/images/icon_scales.png',
+      name: 'スケールリファレンス',
+      description: '各スケールの構成音一覧およびピアノ鍵盤図/ギター指板図を閲覧できます。'
+    },
+  ]
+</script>
+
+<template>
+  <section id="tool-list">
+    <ToolListItem v-for="itemAttrs in itemAttrsList" :attrs="itemAttrs" />
+  </section>
+</template>
+
+<style scoped>
+</style>
